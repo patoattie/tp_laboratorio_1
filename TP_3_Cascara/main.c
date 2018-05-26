abd78913ef160c48aa4d38a7f1820007753d3e08
pattiemecon <patoattie@gmail.com>
2018-05-26 08:57:32 -0300

    TP 3 cascara

---
+++ TP_3_Cascara/main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "funciones.h"
+
+typedef struct{
+    char titulo[20];
+    char genero[20];
+    int duracion;
+    char descripcion[50];
+    int puntaje;
+    char linkImagen[50];
+}EMovie;
+
+int main()
+{
+    char seguir='s';
+    int opcion=0;
+
+    while(seguir=='s')
+    {
+        printf("1- Agregar pelicula\n");
+        printf("2- Borrar pelicula\n");
+        printf("3- Generar pagina web\n");
+        printf("4- Salir\n");
+
+        scanf("%d",&opcion);
+
+        switch(opcion)
+        {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+               break;
+            case 4:
+                seguir = 'n';
+                break;
+        }
+    }
+
+    return 0;
+}
